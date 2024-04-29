c1675880711ce28ab62ffd413b2fe03f457deefc
danieldalbelo <daniel.neves@sptech.school>
2024-04-29 10:42:55 -0300

    Atualizando o script do banco de dados

--- sql-script/database-structure.sql
+++ sql-script/database-structure.sql
@@ -4,31 +4,32 @@ USE soytech;
 -- dados do lead capturado pelos meios de contato
 CREATE TABLE prospect(
 	idProspect INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45),
-    razaoSocial VARCHAR(100),
-    email VARCHAR(100),
-    telefone VARCHAR(13),
-    descricao VARCHAR(500)
+    nome VARCHAR(45) NOT NULL,
+    razaoSocial VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(13) NOT NULL,
+    descricao VARCHAR(500) NOT NULL
 );
 
 -- após o prospect, ele se torna nosso cliente, a empresa que representa o comprador da solução
 -- como representa o "cliente", "ele" possui o acesso principal, o primeiro acesso verificado para a plataforma
 CREATE TABLE empresa(
 	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45),
-    razaoSocial VARCHAR(100),
-    telCorp VARCHAR(13),
-    email VARCHAR(100),
-    cnpj CHAR(14),
+    nome VARCHAR(45) NOT NULL,
+    razaoSocial VARCHAR(100) NOT NULL,
+    telCorp VARCHAR(13) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    cnpj CHAR(14) NOT NULL,
     fkProspect INT,
+    
 		CONSTRAINT fkEmpresaHasProspect FOREIGN KEY (fkProspect) REFERENCES prospect(idProspect)
 );
 
 -- essa empresa possui endereço
 CREATE TABLE enderecoEmpresa(
 	idEnderecoEmpresa INT AUTO_INCREMENT,
-	cep CHAR(8),
-	numEndereco VARCHAR(8),
+	cep CHAR(8) NOT NULL,
+	numEndereco VARCHAR(8) NOT NULL,
     complemento VARCHAR(45),
 	fkEmpresa INT,
 		CONSTRAINT pkEnderecoEmpresa PRIMARY KEY (idEnderecoEmpresa, fkEmpresa),
@@ -38,11 +39,10 @@ CREATE TABLE enderecoEmpresa(
 -- a fazenda que pertence á essa empresa de produção de soja
 -- apenas o acesso "master" pode cadastrar a fazenda ou fazendas
 CREATE TABLE fazenda(
-	idFazenda INT AUTO_INCREMENT,
+	idFazenda INT PRIMARY KEY AUTO_INCREMENT,
 	larguraHec INT,
     alturaHec INT,
 	fkEmpresa INT,
-		CONSTRAINT pkEmpresaHasFazenda PRIMARY KEY (idFazenda,fkEmpresa),
         CONSTRAINT fkEmpresaHasFazenda FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );	
 
@@ -50,20 +50,21 @@ CREATE TABLE fazenda(
 -- cadastrado pelo cliente, usuário "master", autonomia de multiusuários
 CREATE TABLE funcionario(
 	idFuncionario INT AUTO_INCREMENT,
-    nome VARCHAR(45),
-    email VARCHAR(100),
-    senha VARCHAR(255),
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    cargo VARCHAR(45),
 	fkEmpresa INT,
 		CONSTRAINT pkEmpresaHasFuncionario PRIMARY KEY (idFuncionario, fkEmpresa),
 		CONSTRAINT fkEmpresaHasFuncionario FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa),
-	fkRepresentante INT,
-    CONSTRAINT fkfuncionario FOREIGN KEY (fkRepresentante) REFERENCES funcionario(idFuncionario)
+	fkMaster INT,
+    CONSTRAINT fkfuncionario FOREIGN KEY (fkMaster) REFERENCES funcionario(idFuncionario)
 );
 
 -- sensor cadastrado onde pertence a uma fazenda
 -- simulando sua localização apenas por meio de eixo x e y
 CREATE TABLE sensor(
-	idSensor INT AUTO_INCREMENT,
+	idSensor INT PRIMARY KEY AUTO_INCREMENT,
     tipo VARCHAR(5) NOT NULL,
 		CONSTRAINT chkTipo CHECK (tipo in('dht11','lm35')),
 	eixoX INT,
@@ -71,7 +72,6 @@ CREATE TABLE sensor(
     minIdeal DECIMAL(5,2) NOT NULL,
     maxIdeal DECIMAL(5,2) NOT NULL,
 	fkFazenda INT,
-		CONSTRAINT pkFazendaHasSensor PRIMARY KEY (idSensor,fkFazenda),
 		CONSTRAINT fkFazendaHasSensor FOREIGN KEY (fkFazenda) REFERENCES fazenda(idFazenda)
 );
 
@@ -79,7 +79,7 @@ CREATE TABLE sensor(
 -- que pertence a algum sensor
 CREATE TABLE sensorLog(
 	idSensorLog INT AUTO_INCREMENT,
-    dadoCapturado DECIMAL(5,2),
+    dadoCapturado DECIMAL(5,2) NOT NULL,
     dataHora DATETIME,
 	fkSensor INT,
 		CONSTRAINT pkSensorHasSensorLog PRIMARY KEY (idSensorLog,fkSensor),
